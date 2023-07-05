56b9cd080632abed31c9778ad3eddfc232d43f3a
Shalom Harpaz <sh770h@gmail.com>
2023-07-05 13:10:50 +0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import CounterComponent from "./components/CounterComponent";
-// import Header from "./components/Header";
+import Header from "./components/Header";
 import TotalComponent from "./components/TotalComponent";
-// import Main from "./components/Main";
-// import CounterComponent from "./components/CounterComponent";
-// import TotalComponent from "./components/TotalComponent";
+import Main from "./components/Main";
 import { TotalProvider } from "./components/context";
-// import TotalComponent from "./components/TotalComponent";
 import Counter from "./components/counter";
 
 function App() {
   return (
     <TotalProvider>
       <div>
-        {/* <Header /> */}
-        {/* <Main /> */}
-        {/* <Footer /> */}
+        <Header />
+        <Main />
+        <Footer />
         <Counter />
         <CounterComponent />
         <TotalComponent />
