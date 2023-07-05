576e2f75cc3b098394a63c59f26746447145a683
Shalom Harpaz <sh770h@gmail.com>
2023-07-05 13:57:19 +0300

    c

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import TotalComponent from "./components/TotalComponent";
 import Main from "./components/Main";
 import { TotalProvider } from "./components/context";
-import Counter from "./components/counter";
+import Counter from "./components/Counter";
 
 function App() {
   return (
